f8f926dfa76b19d976add239c43e8631ed025ce4
yared123 <106662261+yarednigus@users.noreply.github.com>
2024-12-28 13:51:24 +0300

    alx set

---
+++ 0x00-ES6_basic/babel.config.js
@@ -0,0 +1,28 @@
+
+module.exports = {
+<<<<<<< HEAD
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+  ],
+};
+
+=======
+    presets: [
+        [
+            '@babel/preset-env',
+            {
+                targets: {
+                    node: 'current',
+                },
+            },
+        ],
+    ],
+};
+>>>>>>> de36966c5ec71039dd091a1af27d98e5d50aa81e
